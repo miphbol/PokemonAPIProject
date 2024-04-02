1edb0678606e09b4c69de6479b843206c30b8e84
Mitch Boling <mphboling@gmail.com>
2024-04-01 22:16:24 -0400

    Pulled base stat data & sprite from API, stylized page, created sound button for sprite

--- pokemon-fetch.js
+++ pokemon-fetch.js
@@ -1,7 +1,9 @@
 let heracrossInfo = document.getElementById('table');
 
+
 let spriteImage = document.getElementById('sprite');
 
+let heracrossStats = document.getElementById('stats');
 
 var aud = document.getElementById("heracrossbutton").children[0];
     var isPlaying = false;
@@ -30,6 +32,7 @@ fetch("https://pokeapi.co/api/v2/pokemon/heracross/")
         let heracrossHeight = json.height;
         let heracrossWeight = json.weight;
 
+
         let listName = document.getElementById("tableHeracrossName");
         listName.width = '80px';
         listName.innerText = heracrossName;
@@ -41,7 +44,7 @@ fetch("https://pokeapi.co/api/v2/pokemon/heracross/")
         heracrossInfo.appendChild(listID);
 
         let listHeight = document.getElementById("tableHeracrossHeight");
-        listHeight.width = '50px';
+        listHeight.width = '80px';
         listHeight.innerText = heracrossHeight;
         heracrossInfo.appendChild(listHeight);
 
@@ -50,8 +53,47 @@ fetch("https://pokeapi.co/api/v2/pokemon/heracross/")
         listWeight.innerText = heracrossWeight;
         heracrossInfo.appendChild(listWeight);
 
+//---------------------------------------------------------//
+
+        let statHP = json.stats[0].base_stat;
+        let statATK = json.stats[1].base_stat;
+        let statDEF = json.stats[2].base_stat;
+        let statSPATK = json.stats[3].base_stat;
+        let statSPDEF = json.stats[4].base_stat;
+        let statSPE = json.stats[5].base_stat;
+
+        let listHP = document.getElementById("heracrossHP");
+        listHP.innerText = statHP;
+        heracrossStats.appendChild(listHP);
+
+        let listATK = document.getElementById("heracrossATK");
+        listATK.innerText = statATK;
+        heracrossStats.appendChild(listATK);
+
+        let listDEF = document.getElementById("heracrossDEF");
+        listDEF.innerText = statDEF;
+        heracrossStats.appendChild(listDEF);
+
+        let listSPATK = document.getElementById("heracrossSPATK");
+        listSPATK.innerText = statSPATK;
+        heracrossStats.appendChild(listSPATK);
+
+        let listSPDEF = document.getElementById("heracrossSPDEF");
+        listSPDEF.innerText = statSPDEF;
+        heracrossStats.appendChild(listSPDEF);
+
+        let listSPE = document.getElementById("heracrossSPE");
+        listSPE.innerText = statSPE;
+        heracrossStats.appendChild(listSPE);
+
+//----------------------------------------------------------//
+
         console.log(heracrossSprite);
         spriteImage.src = heracrossSprite;
     })
+
+
+
+
 
 
